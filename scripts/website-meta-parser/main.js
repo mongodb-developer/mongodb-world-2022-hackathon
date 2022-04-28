f5352d6afb7aae4e9bf52135ef6679a57cd63740
Nic Raboy <nic@nraboy.com>
2022-04-28 16:16:30 -0700

    improve upon the website parsing script

--- scripts/website-meta-parser/main.js
+++ scripts/website-meta-parser/main.js
@@ -14,41 +14,82 @@ const mongoClient = new MongoClient(process.env.ATLAS_URI);
 
         // Find All Documents Without News Website Information
 
-        const newsWithoutInfo = await collection
-            .find(
-                { 
-                    "Info": { 
-                        "$exists": false 
-                    },
-                    "SourceURL": {
-                        "$exists": true
+        for(let j = 0; j < 3; j++) {
+            const newsWithoutInfo = await collection
+                .find(
+                    { 
+                        "Info": { 
+                            "$exists": false 
+                        },
+                        "SourceURL": {
+                            "$exists": true
+                        }
                     }
-                }
-            )
-            .project(
-                { 
-                    "_id": 1, 
-                    "SourceURL": 1 
-                }
-            )
-            .limit(50)
-            .toArray();
+                )
+                .project(
+                    { 
+                        "_id": 1, 
+                        "SourceURL": 1 
+                    }
+                )
+                .limit(50)
+                .toArray();
 
-        for(let i = 0; i < newsWithoutInfo.length; i++) {
-            let result = await parser(newsWithoutInfo[i].SourceURL);
-            let metaData = { meta: result.meta, og: result.og };
-            await collection.updateOne(
-                {
-                    "_id": newsWithoutInfo[i]._id
-                },
-                {
-                    "$set": {
-                        "Info": metaData
-                    }
-                }
-            );
+            if(newsWithoutInfo.length == 0) return;
+
+            var documentsToPopulate = [];
+
+            console.log(`Retrieved Batch ${j}`);
+
+            for(let i = 0; i < newsWithoutInfo.length; i++) {
+                documentsToPopulate.push(
+                    parser(newsWithoutInfo[i].SourceURL)
+                        .then(result => {
+                            // return {
+                            //     "_id": newsWithoutInfo[i]._id,
+                            //     "SourceURL": newsWithoutInfo[i].SourceURL,
+                            //     "meta": result.meta,
+                            //     "og": result.og
+                            // };
+                            return collection.updateOne(
+                                {
+                                    "_id": newsWithoutInfo[i]._id
+                                },
+                                {
+                                    "$set": {
+                                        "Info": {
+                                            "meta": result.meta,
+                                            "og": result.og
+                                        }
+                                    }
+                                }
+                            );
+                        }, error => {
+                            if(error.response && error.response.status) {
+                                if(![403, 404].includes(error.response.status)) {
+                                    console.error(error.message);
+                                    // collection.updateOne(
+                                    //     {
+                                    //         "_id": newsWithoutInfo[i]._id
+                                    //     },
+                                    //     {
+                                    //         "$set": {
+                                    //             "IsSourceURLDead": true
+                                    //         }
+                                    //     }
+                                    // )
+                                }
+                            }
+                        })
+                );
+            }
+
+            let result = await Promise.all(documentsToPopulate);
+
+            // console.log(result);
+
+            console.log(`Batch ${j} Updated`);
         }
-
     } catch (e) {
         console.error(e);
     } finally {
