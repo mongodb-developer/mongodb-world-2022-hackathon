2b7c3148295fbb21fe5d69e11e59d9beba9984e4
Judy2k <judy@judy.co.uk>
2022-05-12 19:27:46 +0100

    Add Realm functions to update GDELT data.

---
+++ realm_functions/gdeltUpdater.js
@@ -0,0 +1,22 @@
+exports = async function(){
+  const AdmZip = require("adm-zip");
+  const http = context.http;
+  
+  const lastUpdateURL = "http://data.gdeltproject.org/gdeltv2/lastupdate.txt";
+  const lastUpdate = (await http.get({ url: lastUpdateURL })).body.text();
+  const exportMatch = lastUpdate.match(/http:\/\/data.gdeltproject.org\/gdeltv2\/(\d+).export.CSV.zip/)
+  const csvURL = exportMatch[0];
+  const downloadId = exportMatch[1];
+  
+  const latestCSV = (await http.get({ url: csvURL })).body;
+  const zip = new AdmZip(new Buffer(latestCSV.toBase64(), 'base64'));
+  const csvData = zip.getEntries()[0].getData().toString('utf-8');
+  const csvLines = csvData.split("/n");
+  
+  if (csvLines[csvLines.length - 1] === "") {
+    csvLines.pop();
+  }
+  const rows = csvLines.map((line) => line.split("\t"));
+  await context.functions.execute("insertCSVRows", rows, downloadId);
+
+};
